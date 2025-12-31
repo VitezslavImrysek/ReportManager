735871d93d54a629e8a30ae1f31f398ab623381d
Vítězslav Imrýšek <vitai7392@gmail.com>
2025-12-31 20:56:41 +0100

    Add column detail editor in columns tab

--- tools/ReportAdmin.App/ViewModels/ReportColumnViewModel.cs
+++ tools/ReportAdmin.App/ViewModels/ReportColumnViewModel.cs
@@ -1,10 +1,26 @@
 ﻿using ReportAdmin.Core;
 using ReportAdmin.Core.Models.Definition;
+using ReportManager.Shared.Dto;
+using System.Collections.ObjectModel;
 
 namespace ReportAdmin.App.ViewModels
 {
     public class ReportColumnViewModel : NotificationObject
     {
-        public ReportColumnUi? Column { get; set => SetValue(ref field, value); }
+        public ReportColumnUi? Column
+        {
+            get => field;
+            set
+            {
+                if (SetValue(ref field, value))
+                {
+                    OnPropertyChanged(nameof(HasColumn));
+                }
+            }
+        }
+
+        public ObservableCollection<ReportColumnType> ColumnTypeValues { get; set => SetValue(ref field, value); } = new();
+
+        public bool HasColumn => Column != null;
     }
 }
