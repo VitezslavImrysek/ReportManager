e57412f21c13615ad1d35998b3778d2460ad1bc6
Vítězslav Imrýšek <vitezslav.imrysek@outlook.cz>
2025-12-31 20:40:25 +0100

    Placeholder

---
+++ tools/ReportAdmin.App/ViewModels/ReportColumnViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ReportAdmin.Core;
+using ReportAdmin.Core.Models.Definition;
+
+namespace ReportAdmin.App.ViewModels
+{
+    public class ReportColumnViewModel : NotificationObject
+    {
+        public ReportColumnUi? Column { get; set => SetValue(ref field, value); }
+    }
+}
